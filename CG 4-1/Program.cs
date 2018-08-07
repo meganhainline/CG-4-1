54bb80976582aecfdd731035189c2d693c93b2bb
Megan Hainline <meg.hainline@gmail.com>
2018-08-06 20:34:32 -0500

    Add project files.

---
+++ CG 4-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
