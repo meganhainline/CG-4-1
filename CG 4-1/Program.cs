f1f806d9d9cdd190bba1e44b47cf6bb6820860cc
Megan Hainline <meg.hainline@gmail.com>
2018-08-11 15:14:51 -0500

    github

--- CG 4-1/Program.cs
+++ CG 4-1/Program.cs
@@ -6,7 +6,21 @@ namespace CG_4_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Your Age");
+            int age = Int32.Parse(Console.ReadLine());
+
+            int[] numbers = new int[21]; //using 21 bc it creates an array going from 0-20
+            
+            for (int i = 2; i <= 20 ; i++) { 
+
+            numbers[i] = age * i;
+            }
+            for (int i = 2; i <= 20; i++)
+            {
+                Console.WriteLine(numbers[i]);
+                
+            }
+            Console.ReadLine();
         }
     }
 }
